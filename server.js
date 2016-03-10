be1d3c6cb502ac0eb6dbac009b4c5ece88534b80
for1traub@gmail.com <eric.weintraub@highpointsolutions.com>
2016-03-10 13:37:43 -0500

    Minor formatting changes

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+// Mongo General Info
+var mongo = require('mongodb');
+var uri = 'mongodb://hps-dbuser:!Collab01@ds064628.mlab.com:64628/trackerdb';
+
+
+
+// Set up MongoDB connection
+
+
 // Specify the port
 var port = process.env.port || 8080;
 
-// Identify root folderapp
+// Identify root folder
 app.use('/',express.static(__dirname + '/public'));
 
 // MAke it go
