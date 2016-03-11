2ed27bde54884c7ecd356d9e5496f181c374d925
for1traub@gmail.com <eric.weintraub@highpointsolutions.com>
2016-03-10 20:44:39 -0500

    Added GET and POST REST endpoints for isses and projects

--- server.js
+++ server.js
@@ -1,22 +1,137 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+// var mongoose = require('mongoose');
+
 
 // Mongo General Info
-var mongo = require('mongodb');
+var mongodb = require('mongodb');
 var uri = 'mongodb://hps-dbuser:!Collab01@ds064628.mlab.com:64628/trackerdb';
 
-
-
-// Set up MongoDB connection
-
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Specify the port
 var port = process.env.port || 8080;
 
-// Identify root folder
-app.use('/',express.static(__dirname + '/public'));
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-// MAke it go
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// HERE ARE MY ROUTES ==========================================================
+
+// GET ALL PROJECTS
+router.route('/projects')
+    .get(function(req,res){
+        var d = [];
+        mongodb.MongoClient.connect(uri, function(err, db) {
+            var projects = db.collection('projects');
+            projects.find ().toArray(function(err,docs){
+                if(err) throw err;
+                res.json(docs);
+            });
+        });
+    });
+// GET ALL ISSUES
+router.route('/issues')
+    .get(function(req,res){
+        var d = [];
+        mongodb.MongoClient.connect(uri, function(err, db) {
+            var projects = db.collection('issues');
+            projects.find ().toArray(function(err,docs){
+                if(err) throw err;
+                res.json(docs);
+            });
+        });
+    });
+
+// SEEDING
+router.route('/seeding')
+    .post(function(req, res){
+        mongodb.MongoClient.connect(uri, function(err, db) {
+            if(err) throw err;
+            var projects = db.collection('issues');
+            var seed = [];
+            projects.insert(seed,function(err,result){
+                if(err){
+                    console.log(err);
+                     db.close();
+                } else {
+                    res.json(result);
+                     db.close();
+                }
+            });
+        });
+    });
+// ADD A PROJECT
+    router.route('/projects')
+        .post(function(req, res){
+            mongodb.MongoClient.connect(uri, function(err, db) {
+                if(err) throw err;
+                var projects = db.collection('projects');
+                var obj = {
+                    project:req.body.project,
+                    client:req.body.client
+                };
+                projects.insert(obj,function(err,result){
+                    if(err){
+                        console.log(err);
+                         db.close();
+                    } else {
+                        console.log(result);
+                         db.close();
+                    }
+                });
+            });
+        });
+
+// ADD AN ISSUE
+    router.route('/issues')
+        .post(function(req, res){
+            mongodb.MongoClient.connect(uri, function(err, db) {
+                if(err) throw err;
+                var projects = db.collection('projects');
+                var obj = {
+                    project:req.body.project,
+                    issueTitle:req.body.issueTitle,
+                    issueDescription:req.body.description,
+                    currentAssignedTo:{
+                            id:req.body.assignedToId,
+                            title:req.body.assignedToTitle
+                    },
+                    dateReported:req.body.dateReported,
+                    reportedBy:req.body.reportedBy,
+                    lastUpdated:new Date().toISOString(),
+                    status:"Open",
+                    priority:req.body.priority,
+                    type:req.body.type
+                };
+                projects.insert(obj,function(err,result){
+                    if(err){
+                        console.log(err);
+                         db.close();
+                    } else {
+                        console.log(result);
+                         db.close();
+                    }
+                });
+            });
+        });
+
+
+// ROUTES -------------------------------
+app.use('/api', router);
+app.use('/',express.static(__dirname + '/public/'));
+
+
 app.listen(port);
-console.log('Listening on port: ',port);
+console.log('Server Running');
